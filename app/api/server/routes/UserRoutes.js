b191708c26db57e722b59e17bec7cc69eb9d2f7a
Brisa Garcia <brisagarciaglz@gmail.com>
2021-07-11 17:23:10 -0600

    Revert "mvc is setup"
    This reverts commit 8725aee19b3678a4c5c93520a4a7b43344f4ffcf.

--- app/api/server/routes/UserRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import UserController from '../controllers/UserController';
-
-const router = Router();
-
-router.get('/:id', UserController.getAUser);
-
-export default router;
