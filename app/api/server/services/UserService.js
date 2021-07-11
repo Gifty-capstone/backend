a007110d3c7a1b83fe6cf2d74497264ad930a779
Joey Haas <75232735+joeyh92989@users.noreply.github.com>
2021-07-11 16:06:51 -0600

    mvc is setup

---
+++ app/api/server/services/UserService.js
@@ -0,0 +1,16 @@
+import database from './app/database/models';
+
+class UserService {
+  static async getAUser(id) {
+    try {
+      const theUser = await database.User.findOne({
+        where: { id: Number(id) },
+      });
+      return theUser;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default UserService;
