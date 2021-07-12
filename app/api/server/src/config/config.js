94d3740433be08e80e501fdb5c0f1800ffa1914d
Brisa Garcia <brisagarciaglz@gmail.com>
2021-07-12 10:14:35 -0600

    Change config vars

--- app/api/server/src/config/config.js
+++ app/api/server/src/config/config.js
@@ -21,10 +21,11 @@ module.exports = {
   },
 
   production: {
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    host: process.env.PRODUCTION_DATABASE_URL,
-    dialect: 'postgres'
+
+    // database: process.env.DB_NAME,
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASS,
+    host: process.env.DATABASE_URL,
+    // dialect: 'postgres'
   }
 };
